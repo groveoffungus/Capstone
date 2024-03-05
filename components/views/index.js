05cd80600dbb19305d3595647a742abf3e35a4e0
Gene Agnew <gragnew@groveoffungus.com>
2024-03-05 14:41:36 -0900

    made changes to capstone, part of homework week 6

---
+++ components/views/index.js
@@ -0,0 +1,6 @@
+export { default as About } from "./About";
+export { default as Main } from "./Main";
+export { default as Contact } from "./Contact";
+export { default as Admin } from "./Admin";
+export { default as Project } from "./Project";
+export { default as Viewnotfound } from "./Viewnotfound";
