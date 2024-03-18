c1fefc258445f9665354e7135333f170bfed6820
Gene Agnew <153901177+groveoffungus@users.noreply.github.com>
2024-03-17 16:51:58 -0800

    Revert "nav working and css being worked on"

--- components/views/Main.js
+++ components/views/Main.js
@@ -2,17 +2,6 @@ import html from "html-literal";
 
 export default () => html`
   <section id="main">
-    <h2>Place Holder for Main</h2>
-    <p>
-      Etiam egestas consectetur gravida. Nulla mollis suscipit sapien sed
-      fermentum. Integer vitae eros a magna vulputate aliquam. Suspendisse sed
-      pulvinar augue, auctor mollis lectus. Class aptent taciti sociosqu ad
-      litora torquent per conubia nostra, per inceptos himenaeos. Duis eleifend
-      diam quis libero sollicitudin efficitur. Nullam sapien eros, tempor eget
-      vulputate ut, interdum vel orci. Donec sit amet tempor mi. Nam feugiat
-      cursus egestas. Suspendisse eget orci et ex mattis ornare tempor non
-      tellus. Suspendisse gravida neque in urna congue bibendum. Duis dui odio,
-      pharetra nec odio ac, ornare vulputate nibh.
-    </p>
+    <h2>Place Holder</h2>
   </section>
 `;
