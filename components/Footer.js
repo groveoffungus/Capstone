454acd39130481392dfa1f5a692169c4ba10ef58
Gene Agnew <153901177+groveoffungus@users.noreply.github.com>
2024-03-17 16:49:25 -0800

    Revert "reinstall npm items"

--- components/Footer.js
+++ components/Footer.js
@@ -2,6 +2,6 @@ import html from "html-literal";
 
 export default () => html`
   <footer>
-    The Cultural Archiver &copy; 2024 <a href="https:www.groveoffungus.com">(Gene Agnew)</a>
+    &copy; 2024 <a href="https://github.com/groveoffungus">Gene Agnew</a>
   </footer>
 `;
